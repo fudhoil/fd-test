055f99201a38e664cf7d31e5297f47a8afa87156
fudhoil <fudhoilhlf@gmail.com>
2023-08-24 15:27:28 +0700

    [fx] card client rt.

--- components/charts/client_retention_chart.js
+++ components/charts/client_retention_chart.js
@@ -3,7 +3,9 @@ import React from 'react';
 const ClientRetentionChart = () => {
     return (
         // TODO: change this img to real Chart
-        <img src="/cards/client-retention-card.png" alt="client retention" className="max-w-full -ml-5" />
+        <div className='bg-[#1C243F] rounded-lg flex flex-row flex-grow justify-between max-w-full gap-2 inter mt-5'>
+            <img src="/cards/client-retention-card.png" alt="client retention" className="max-w-full" />
+        </div>
     );
 }
 
